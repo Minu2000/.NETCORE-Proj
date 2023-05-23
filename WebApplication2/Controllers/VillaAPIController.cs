3156ec127c739c0d0c346929e7c842cb3577519b
MINUMITHRA BALACHANDAR <minumithra.balachandar@gavait.com>
2023-05-23 07:47:05 +0530

    added Cors policy and models folder

---
+++ WebApplication2/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    [Route("/api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1, Name="minu"},
+                new Villa {Id =2 , Name =" narmadha"}
+            };
+            
+        }
+    }
+}
